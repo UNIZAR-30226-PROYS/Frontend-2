5047affb04d269da0519445823290caa7b8a2c54
alexOarga <alex718123@gmail.com>
2018-05-10 19:47:54 +0200

    wave

--- resources/js/main.js
+++ resources/js/main.js
@@ -39,3 +39,24 @@ function getCookie(cname) {
     }
     return "";
 }
+
+
+
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
